389eecd00a1081bdf401dfcc82b04a340e31c6db
Sirjan <sharsir.ss@gmail.com>
2016-08-30 10:52:51 +0545

    Add Subject In Messagees

--- database/migrations/2016_08_06_135732_create_messages_table.php
+++ database/migrations/2016_08_06_135732_create_messages_table.php
@@ -15,6 +15,7 @@ class CreateMessagesTable extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
+            $table->string('subject');
             $table->text('message');
             $table->timestamps();
 
